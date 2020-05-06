3f2178b8b7df24c70f7610530dc3bd7abb44269e
Romain <romainmart.85@gmail.com>
2020-05-06 15:55:57 +0200

    essai

---
+++ js/ex.js
@@ -0,0 +1,17 @@
+let numberOfCats = 2;
+
+let numberOfDogs = 4;
+
+let numberOfPets = numberOfCats + numberOfDogs;  // - ; * ; /
+
+let cookies = 10;
+cookies -= 2;           //8
+cookies +=12;           //20
+
+let numberCookies = 10;
+numberCookies++;            //11
+numberCookies--;            //10
+
+let poop = 2;
+poop *= 6;      //12
+poop /= 3;      //4
---
+++ js/info.js
@@ -0,0 +1,11 @@
+let numberOfSeasons = 6;
+let numberOfEpisodes = 12;
+
+let episodeTime = 45;
+let commercialTime = 5;
+
+let totalShowTime = (episodeTime + commercialTime) * numberOfSeasons * numberOfEpisodes;
+
+let paragraph = document.querySelector('#info');
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season
+Total viewing time: ${totalShowTime} minutes`
